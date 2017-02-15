f8bfa55ef2dfe1f16aa9a2c06a485dce0d5a13eb
J. Cohen <hostelmarketing@gmail.com>
2017-02-15 10:36:36 -0800

    More code

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,17 +1,20 @@
 <template>
     <div id="app">
-        <navbar></navbar>
+        <app-navbar></app-navbar>
         <router-view></router-view>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
     import Navbar from './components/ui/Navbar.vue';
+    import Footer from './components/ui/Footer.vue';
 
     export default {
         name: 'app',
         components: {
-            'navbar': Navbar
+            'app-navbar': Navbar,
+            'app-footer': Footer
         }
     };
 </script>
---
+++ template/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="about">
+        <h1>{{ msg }}</h1>
+        <p>This is the about page.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'about',
+        data() {
+            return {
+                msg: 'About'
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    h1 {
+        color: darkslateblue;
+    }
+</style>
