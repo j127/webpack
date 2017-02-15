b59f4adff82328b69bc476e32264815742508f78
J. Cohen <hostelmarketing@gmail.com>
2017-02-15 10:28:11 -0800

    More formatting fixes

---
+++ template/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data() {
+            return {
+                msg: 'Saluton Mondo'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: darkslateblue;
+    }
+</style>
