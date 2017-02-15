7c93a16277e18f778518fd4e8400e5ca45fc5c71
J. Cohen <hostelmarketing@gmail.com>
2017-02-15 10:28:25 -0800

    Add more dependencies, start adding Bootstrap 4

--- template/src/components/Home.vue
+++ template/src/components/Home.vue
@@ -1,6 +1,34 @@
 <template>
     <div class="home">
-        <h1>{{ msg }}</h1>
+        <div class="jumbotron">
+            <div class="container">
+                <h1>{{ msg }}</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Heading</h2>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+                </div>
+            </div>
+
+            <hr>
+        </div>
     </div>
 </template>
 
@@ -12,7 +40,7 @@
                 msg: 'Saluton Mondo'
             }
         }
-    }
+    };
 </script>
 
 <style scoped>
---
+++ template/src/components/ui/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        Copyright &copy; {{ currentYear }}
+    </footer>
+</template>
+
+<script>
+    export default {
+        computed: {
+            currentYear: () => {
+                new Date().getFullYear();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    footer {
+        color: #ccc;
+    }
+</style>
