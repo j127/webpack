7013e292717b0ba88236ddc4f5474a127947cbaf
J. Cohen <hostelmarketing@gmail.com>
2017-02-15 10:52:05 -0800

    Add jQuery

--- template/src/App.vue
+++ template/src/App.vue
@@ -7,8 +7,13 @@
 </template>
 
 <script>
+    import jQuery from 'jquery';
     import Navbar from './components/ui/Navbar.vue';
     import Footer from './components/ui/Footer.vue';
+
+    // Make jQuery available to the entire page
+    // TODO: Improve this or remove jQuery
+    const $ = window.$ = window.jQuery = jQuery;
 
     export default {
         name: 'app',
